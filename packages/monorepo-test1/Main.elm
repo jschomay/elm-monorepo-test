7e0f2e3bc6ec322954d021c73ec33a29544f9706
Jeff Schomay <jschomay@gmail.com>
2017-01-11 17:48:16 -0700

    set up test monorepo structure
    but this doesn't work, because the `packages/monorepo-test1/elm-package.json`'s
    `repository` field breaks things because elm wants the format to be
    `https://github.com/user/repo.git`, no more no less

---
+++ packages/monorepo-test1/Main.elm
@@ -0,0 +1,9 @@
+module Main exposing (main)
+import Html exposing (Html, text)
+
+{- Can you make a monorepo in Elm?  This would be one "package" under https://github.com/jschomay/elm-monorepo-test, but published as its own package.
+-}
+
+main : Html msg
+main =
+  text "Hello, World!"
